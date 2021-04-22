492a1cdff04583ed0b8fbc69f876457168a780af
tjwilks <tobywilkinson3@gmail.com>
2021-04-22 20:39:17 +0100

    hyperparameter search and feature selection documentation

--- src/modelling/feature_selection.py
+++ src/modelling/feature_selection.py
@@ -1,22 +1,47 @@
 from sklearn.ensemble import RandomForestClassifier
 
 
-def feature_selection(X_data,
-                      y_data,
-                      col_na_proportion,
-                      missing_value_filter,
-                      feature_importance_rank_filter):
-
-    feature_selector = FeatureSelector(X_data, y_data, col_na_proportion, missing_value_filter, feature_importance_rank_filter)
-    feature_selector.drop_cols_with_x_prop_missing_values()
-    feature_selector.random_forrest_feature_importances()
-    feature_selector.feature_importance_feature_selection()
-    feature_selector.filter_X_data_to_selected_features()
-
-    return feature_selector.X_data, feature_selector.selected_features
-
-
 class FeatureSelector:
+    """
+        A class for searching selecting features based on
+        missing value proportion or feature importance rank
+        methods
+
+        Attributes
+        ----------
+        X_data : pandas dataframe
+            pandas dataframe containing data to be used for training
+            on and predicting output variable y_data
+
+        y_data : numpy array
+            numpy array containing output variable to be predicted by
+            X_data
+
+        col_na_proportion : dict
+            dictionary containing the proportion of missing values
+            present in each variable within X_data for the purpose of
+            missing value based feature selection
+
+        missing_value_filter : float
+            threshold to above which columns that had a greater
+            proportion of missing values before imputation (as
+            is recorded in col_na_proportion) are removed
+
+        feature_importance_rank_filter : int
+            threshold bellow which columns with a lower
+            feature importance rank are removed
+
+        Methods
+        -------
+        select_on_missing_value_proportion
+            removes columns that had a proportion of missing
+            values before imputation (as is recorded in
+            col_na_proportion) above threshold set by missing_value_filter
+
+        select_on_feature_importance_rank
+            removes columns that have a lower feature importance rank
+            than threshold set by feature_importance_rank_filter
+        """
 
     def __init__(self, X_data, y_data, col_na_proportion, missing_value_filter, feature_importance_rank_filter):
         self.X_data = X_data
@@ -26,13 +51,13 @@ class FeatureSelector:
         self.feature_importance_rank_filter = feature_importance_rank_filter
         self.feature_importance_dict = None
 
-    def drop_cols_with_x_prop_missing_values(self):
+    def select_on_missing_value_proportion(self):
         cols_na_proportion_above_filter = self.col_na_proportion[self.col_na_proportion > self.missing_value_filter]
         cols_with_x_prop_missing_values = cols_na_proportion_above_filter.index
         cols_to_drop = set(cols_with_x_prop_missing_values).intersection(set(self.X_data.columns))
         self.X_data = self.X_data.drop(cols_to_drop, axis=1)
 
-    def random_forrest_feature_importances(self):
+    def select_on_feature_importance_rank(self):
         model = RandomForestClassifier(n_estimators=200, max_features=5, max_leaf_nodes=25, n_jobs=-1, oob_score=True)
         model.fit(self.X_data.to_numpy(), self.y_data)
         feature_importances = model.feature_importances_
@@ -41,11 +66,7 @@ class FeatureSelector:
         for feature_importance, feature in sorted(zip(feature_importances, self.X_data.columns), reverse=True):
             self.feature_importance_dict[feature] = (importance_rank, feature_importance)
             importance_rank += 1
-
-    def feature_importance_feature_selection(self):
         self.selected_features = [feature for feature, rank_score in self.feature_importance_dict.items() if
                                   rank_score[0] <= self.feature_importance_rank_filter]
-
-    def filter_X_data_to_selected_features(self):
         self.X_data = self.X_data.loc[:, self.selected_features]
         self.X_data = self.X_data.to_numpy()
--- src/modelling/total_grid_search.py
+++ src/modelling/total_grid_search.py
@@ -3,7 +3,7 @@ from sklearn.base import BaseEstimator
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import unique_labels
 from sklearn.ensemble import RandomForestClassifier
-from src.modelling import feature_selection
+from src.modelling.feature_selection import FeatureSelector
 from sklearn.metrics import accuracy_score
 from skopt import BayesSearchCV
 import numpy as np
@@ -11,6 +11,37 @@ from src.utils import read_input_data
 
 
 class HypParamSearch:
+    """
+        A class for searching for optimal hyperparameters using
+        either grid search or bayesian optimisation
+
+        Attributes
+        ----------
+        X_data : pandas dataframe
+            pandas dataframe containing data to be used for training
+            on and predicting output variable y_data
+
+        y_data : numpy array
+            numpy array containing output variable to be predicted by
+            X_data
+
+        col_na_proportion : dict
+            dictionary containing the proportion of missing values
+            present in each variable within X_data for the purpose of
+            missing value based feature selection
+
+        Methods
+        -------
+        grid_hyp_search(grid_search_config=dict)
+            conducts grid search with across hyperparameter range
+            specified in grid_search_config (as controlled by
+            grid_search_param_grid.json)
+
+        bayes_opt_hyp_search(bayes_opt_search_config=dict)
+            conducts bayesian optimisation hyperparameter search
+            across range specified in bayes_opt_search_config (as
+            controlled by bayes_opt_search_param_grid.json)
+        """
 
     def __init__(self, X_data, y_data, col_na_proportion):
         self.X_data = X_data
@@ -18,6 +49,14 @@ class HypParamSearch:
         self.fit_params = {"col_na_proportion": col_na_proportion}
 
     def grid_hyp_search(self, grid_search_config):
+        """
+        Parameters
+        ----------
+        grid_search_config : dict
+            conducts grid search with across hyperparameter range
+            specified in grid_search_config (as controlled by
+            grid_search_param_grid.json)
+        """
         custom_estimator = TotalEstimator()
         grid_search_param_grid = read_input_data.read_json_to_dict(grid_search_config["param_grid_config_path"])
         grid_search_param_grid = {key: list(np.arange(value[0], value[1], value[2]))
@@ -29,6 +68,14 @@ class HypParamSearch:
         return grid_search_cv_preprocess
 
     def bayes_opt_hyp_search(self, bayes_opt_search_config):
+        """
+        Parameters
+        ----------
+        bayes_opt_search_config : dict
+            conducts bayesian optimisation hyperparameter search
+            across range specified in bayes_opt_search_config (as
+            controlled by bayes_opt_search_param_grid.json)
+        """
         custom_estimator = TotalEstimator()
         grid_search_param_grid = read_input_data.read_json_to_dict(bayes_opt_search_config["param_grid_config_path"])
         grid_search_param_grid = {key: tuple(value) for key, value in grid_search_param_grid.items()}
@@ -42,6 +89,16 @@ class HypParamSearch:
 
 
 def get_grid_search_results(grid_search_cv_preprocess):
+    """
+        function for returning hyperparameter search results to
+        console
+
+        Parameters
+        ----------
+        grid_search_cv_preprocess: object
+            fitted GridSearchCV or BayesSearchCV hyperparameter
+            optimisation object
+    """
     grid_search_cv_results = grid_search_cv_preprocess.cv_results_
     for scores, parameters in zip(grid_search_cv_results["mean_test_score"], grid_search_cv_results["params"]):
         print(f"Cross-val error: {np.round(scores, 5)} --- Parameters: {parameters}")
@@ -51,6 +108,65 @@ def get_grid_search_results(grid_search_cv_preprocess):
 
 
 class TotalEstimator(BaseEstimator):
+    """
+        A custom scikit-learn estimator that varies preprocessing
+        and modelling based on specified hyperparameters
+        and produces associated predictions and error scores
+
+        Attributes
+        ----------
+        missing_value_filter : float
+            threshold to above which columns that had a greater
+            proportion of missing values before imputation (as
+            is recorded in col_na_proportion) are removed
+
+        feature_importance_rank_filter : int
+            threshold bellow which columns with a lower
+            feature importance rank are removed
+
+        n_estimators : int
+            The number of trees in the forest used in random
+            forrest model
+
+        max_features : {"auto", "sqrt", "log2"}, int or float, default="auto"
+            The number of features to consider when looking for the best split:
+
+            - If int, then consider `max_features` features at each split.
+            - If float, then `max_features` is a fraction and
+              `int(max_features * n_features)` features are considered at each
+              split.
+            - If "auto", then `max_features=sqrt(n_features)`.
+            - If "sqrt", then `max_features=sqrt(n_features)` (same as "auto").
+            - If "log2", then `max_features=log2(n_features)`.
+            - If None, then `max_features=n_features`.
+
+            Note: the search for a split does not stop until at least one
+            valid partition of the node samples is found, even if it requires to
+            effectively inspect more than ``max_features`` features.
+
+        max_leaf_nodes : int, default=None
+            Grow trees with ``max_leaf_nodes`` in best-first fashion.
+            Best nodes are defined as relative reduction in impurity.
+            If None then unlimited number of leaf nodes.
+
+        fit_params : dict
+            Parameters to pass to the fit method.
+
+        times_fit: int
+            integer for tracking the progress of hyperparameter search
+            by counting number of times fit method is called
+
+        Methods
+        -------
+        fit(X=pandas dataframe, y=numpy array, **fit_params=dict)
+            fit estimator
+
+        predict(X=pandas dataframe)
+            predict based on fitted estimator
+
+        score(X=pandas dataframe, y=numpy array)
+            calculate accuracy score of estimator predictions
+        """
 
     def __init__(self,
                  missing_value_filter=0.5,
@@ -70,25 +186,46 @@ class TotalEstimator(BaseEstimator):
         self.times_fit = times_fit
 
     def fit(self, X, y, **fit_params):
+        """
+        Parameters
+        ----------
+        X : pandas dataframe
+            pandas dataframe containing data to be used for model
+            fitting
+
+        y : numpy array
+            numpy array containing output variable to be used for
+            model fitting
+
+        fit_params : dict
+            dictionary containing additional parameters to be used in
+            model fitting.
+        """
+        # handle fit params depending on whether estimator attribute or
+        # fit method parameter
         self.fit_params = fit_params
         if list(self.fit_params.keys())[0] == "fit_params":
             col_na_proportion = self.fit_params["fit_params"]["col_na_proportion"]
         else:
             col_na_proportion = self.fit_params["col_na_proportion"]
 
-        X, self.selected_features = feature_selection.feature_selection(
-            X,
-            y,
-            col_na_proportion,
-            self.missing_value_filter,
-            self.feature_importance_rank_filter
-        )
+        feature_selector = FeatureSelector(X,
+                                           y,
+                                           col_na_proportion,
+                                           self.missing_value_filter,
+                                           self.feature_importance_rank_filter)
+        feature_selector.select_on_missing_value_proportion()
+        feature_selector.select_on_feature_importance_rank()
+        X = feature_selector.X_data
+        self.selected_features = feature_selector.selected_features
 
-        X, y = check_X_y(X, y)
-
+        # if feature selection has removed columns so that number
+        # of columns remaining is less than max features
+        # set max features to be number of columns remaining
         if self.max_features > X.shape[1]:
             self.max_features = X.shape[1]
 
+        # Random forrest classification
         self.baseline_rf_model = RandomForestClassifier(
             n_estimators=self.n_estimators,
             max_features=self.max_features,
@@ -98,18 +235,40 @@ class TotalEstimator(BaseEstimator):
         )
         self.baseline_rf_model.fit(X, y)
 
+        # required functionality for sci-kit learn estimator
+        X, y = check_X_y(X, y)
         self.classes_ = unique_labels(y)
         self.X_ = X
         self.y_ = y
         return self
 
-    def predict(self, X, y):
+    def predict(self, X):
+        """
+        Parameters
+        ----------
+        X : pandas dataframe
+            pandas dataframe containing data to be used
+            predict output y variable based on fitted
+            estimator model
+        """
         X = X.loc[:, self.selected_features].to_numpy()
         check_is_fitted(self)
         X = check_array(X)
         return self.baseline_rf_model.predict(X)
 
     def score(self, X, y):
-        y_pred = self.predict(X, y)
+        """
+        Parameters
+        ----------
+        X : pandas dataframe
+            pandas dataframe containing data to be used
+            predict output y variable based on fitted
+            estimator model
+
+        y : numpy array
+            output variable to be used to compare against
+            estimator predictions for score to be calculated
+        """
+        y_pred = self.predict(X)
         score = accuracy_score(y, y_pred)
         return score
