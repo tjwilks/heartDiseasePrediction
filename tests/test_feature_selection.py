ee5616410d97b1f8555cabe64fb018f4754555d2
tjwilks <tobywilkinson3@gmail.com>
2021-04-23 18:46:12 +0100

    BUG - function:select_on_missing_value_proportion - test:test_cols_higher_na_prop_above_removed

---
+++ tests/test_feature_selection.py
@@ -0,0 +1,64 @@
+from src.utils import read_input_data
+from src.utils.preprocessing import DataPreprocessor
+from src.modelling.feature_selection import FeatureSelector
+import configparser
+import pytest
+
+
+class TestFeatureSelection:
+    """
+        a class for testing feature selection module
+
+        Methods
+        -------
+        setup_test:
+            initialises FeatureSelector object
+
+        test_cols_dropped_by_missing_value_prop:
+            tests select_on_missing_value_proportion removes columns
+
+        test_cols_dropped_by_feature_importance_rank:
+            tests select_on_feature_importance_rank removes columns
+
+        test_cols_higher_na_prop_above_removed:
+            tests columns remaining are equals to columns bellow
+            missing_value_filter in col_na_proportion dict
+
+    """
+
+    @pytest.fixture(autouse=True)
+    def setup_test(self):
+        self.config = configparser.ConfigParser()
+        self.config.read("config/config.yml")
+        self.config = self.config["read_input_data_config"]
+        self.data = read_input_data.get_data(self.config)
+        data_preprocessor = DataPreprocessor(self.data)
+        data_preprocessor.preprocess_data()
+        self.feature_selector = FeatureSelector(X_data=data_preprocessor.X_data,
+                                                y_data=data_preprocessor.y_data,
+                                                col_na_proportion=data_preprocessor.col_na_proportion,
+                                                missing_value_filter=0.5,
+                                                feature_importance_rank_filter=20)
+
+    def test_cols_dropped_by_missing_value_prop(self):
+        n_cols_before = self.feature_selector.X_data.shape[1]
+        self.feature_selector.select_on_missing_value_proportion()
+        n_cols_after = self.feature_selector.X_data.shape[1]
+        assert n_cols_before > n_cols_after
+
+    def test_cols_dropped_by_feature_importance_rank(self):
+        n_cols_before = self.feature_selector.X_data.shape[1]
+        self.feature_selector.select_on_feature_importance_rank()
+        n_cols_after = self.feature_selector.X_data.shape[1]
+        assert n_cols_before > n_cols_after
+
+    def test_cols_higher_na_prop_above_removed(self):
+        self.feature_selector.select_on_missing_value_proportion()
+        columns_remaining = self.feature_selector.X_data.columns
+        cols_na_prop_bellow = self.feature_selector.col_na_proportion[self.feature_selector.col_na_proportion <=
+                                                                      self.feature_selector.missing_value_filter]
+        cols_na_prop_bellow = cols_na_prop_bellow.index
+        na_bellow_not_remaining = set(cols_na_prop_bellow) - set(columns_remaining)
+        n_cols_na_bellow_not_remaining = len(na_bellow_not_remaining)
+        assert n_cols_na_bellow_not_remaining == 0
+
